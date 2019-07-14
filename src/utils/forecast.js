2c03a58ef5ef3b0e14a2cfcebbccb559496e2aa8
Devang Jain <dj@Devangs-MacBook-Pro-2.local>
2019-07-14 21:53:34 +0530

    Setup app for heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '°C with a ' + body.daily.data[0].precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '°C with a ' + (body.daily.data[0].precipProbability)*100 + '% chance of rain.')
         }
     })
 }
