32f6490683c603370063c14b782d87d9815b56b9
Devang Jain <dj@Devangs-MacBook-Pro-2.local>
2019-07-14 22:46:47 +0530

    views edited

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'This is some helpful text :',
         title: 'Help',
         name: 'Devang Jain'
     })
